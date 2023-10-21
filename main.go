10a9fc89414eaf62dbdc007aec06e8187b0ded03
swastik959 <Sswastik959@gmail.com>
2023-10-21 20:24:24 +0530

    added commands

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"net"
+	"time"
+
 	cache "github.com/swastik959/distributed_chache-/chache"
 )
 
@@ -10,6 +14,14 @@ func main() {
 		IsLeader:   true,
 	}
 
+	go func() {
+		time.Sleep(time.Second * 2)
+		conn, err := net.Dial("tcp", opts.ListenAddr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		conn.Write([]byte("Set Foo Bar 2500"))
+	}()
 	server := NewServer(opts, cache.New())
 	server.Start()
 
